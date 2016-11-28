99de46012cdaf916d7b9f98091a6071fe705c742
Joona Viertola <joona.viertola@druid.fi>
2016-11-28 15:45:11 +0200

    Simple router, root class, simple celect query function and credential dictionary.

---
+++ app/rest.py
@@ -0,0 +1,32 @@
+# example.py
+from pycnic.core import WSGI, Handler
+import psycopg2
+
+db_cred = {
+    'host': '127.0.0.1',
+    'name': 'fast-rest',
+    'user': 'scionar'
+}
+
+# Helper functions for output
+def dbSelect(query):
+    conn = psycopg2.connect(host=db_cred['host'], database=db_cred['name'], user=db_cred['user'])
+    cur = conn.cursor()
+    cur.execute(query)
+    db_result = cur.fetchall()
+    cur.close()
+    conn.close()
+    return db_result
+
+
+class Root(Handler):
+    def get(self, name="Root"):
+        return {
+            "lines" : dbSelect("SELECT * FROM test;")
+        }
+
+class app(WSGI):
+    routes = [
+        ('/', Root()),
+        ('/([\w]+)', Root())
+    ]
